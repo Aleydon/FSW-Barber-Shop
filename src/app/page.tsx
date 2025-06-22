18534e7d250b7cd3fadefcdcae8c60b3826f82f6
Aleydon <aleydonroberto@gmail.com>
2025-06-22 01:35:51 -0300

    chore(shadcn): setup shadcn on project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from './_components/ui/button';
+
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h2 className="bg-amber-300 text-3xl font-bold text-red-500">
         Hello World
       </h2>
+      <Button>Button</Button>
     </div>
   );
 }
